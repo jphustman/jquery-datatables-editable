05ae179fdbe3dca3a7305fea6dbea439c25922a1
jocapc@gmail.com <jocapc@gmail.com@3bbee011-783d-228a-ecdf-7ade593d4de3>
2011-03-25 20:19:39 +0000

    git-svn-id: http://jquery-datatables-editable.googlecode.com/svn/trunk@42 3bbee011-783d-228a-ecdf-7ade593d4de3

--- UpdateData.php
+++
@@ -1,2 +0,0 @@
-Update page is not implemented. In this off-line demo update page always returns this error message. 
-In the real example implement server side page that accepts parameters and return value that has beign edited.
