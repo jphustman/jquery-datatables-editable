5434d3153173d53d82abd2c9fcf9a2cff2a5d5c2
jocapc@gmail.com <jocapc@gmail.com@3bbee011-783d-228a-ecdf-7ade593d4de3>
2011-05-25 22:39:03 +0000

    git-svn-id: http://jquery-datatables-editable.googlecode.com/svn/trunk@142 3bbee011-783d-228a-ecdf-7ade593d4de3

---
+++ ajax_source_2.js
@@ -0,0 +1,12 @@
+{ "aaData": [
+	["Trident","Internet Explorer 4.0","Win 95+"],
+	["Trident","Internet Explorer 5.0","Win 95+"],
+	["Trident","Internet Explorer 5.5","Win 95+"],
+	["Trident","Internet Explorer 6","Win 98+"],
+	["Trident","Internet Explorer 7","Win XP SP2+"],
+	["Misc","Links","Text only"],
+	["Misc","Lynx","Text only"],
+	["Misc","IE Mobile","Windows Mobile 6"],
+	["Misc","PSP browser","PSP"],
+	["Other browsers","All others","-"]
+] }
